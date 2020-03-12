546ea83107cbe889c1e50c873ee56a3491af2a44
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-03-12 23:32:24 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useDimensions from "react-use-dimensions";
+import { getHexagonsToFillZone } from "scripts";
+import { Canvas } from "Canvas";
+import { Hexagon } from "Hexagon";
+import "./styles.css";
 
-function App() {
+const App = () => {
+  const [ref, { width, height }] = useDimensions();
+  const dpr = window.devicePixelRatio || 1;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main ref={ref}>
+      {width === undefined || height === undefined || dpr === undefined ? (
+        <span role="img" aria-label="thinking face">
+          🤔
+        </span>
+      ) : (
+        <Canvas width={width} height={height} dpr={dpr} isAnimating>
+          {getHexagonsToFillZone({
+            height: height * dpr,
+            width: width * dpr,
+          }).map((hexagon, index) => (
+            <Hexagon key={index} {...hexagon} />
+          ))}
+        </Canvas>
+      )}
+    </main>
   );
-}
+};
 
 export default App;
---
+++ src/scripts/hooks.ts
@@ -0,0 +1,14 @@
+import React from "react";
+import { CanvasContext, FrameContext } from "Canvas";
+
+export const useCanvas = () => {
+  React.useContext(FrameContext);
+  const renderingContext = React.useContext(CanvasContext);
+  return renderingContext;
+};
+
+export const useAnimation = <T extends any>(initialValue: T, valueUpdater: (oldValue: T) => T) => {
+  const animatedValue = React.useRef(initialValue);
+  animatedValue.current = valueUpdater(animatedValue.current);
+  return animatedValue.current;
+};
